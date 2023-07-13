44f63747273d830f4513349aaac672cb13761767
sarahcssiqueira <sarahcso@hotmail.com>
2023-07-13 13:34:06 -0300

    feat: new rules to webpack.config.js. For js 'babel-loader' and for css 'css-loader', 'sass-loader' and the plugin 'MiniCSSExtractPlugin'

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,42 @@
  * Webpack configuration.
  */
 
+const path = require('path');
+const MiniCssExtractPlugin = require( "mini-css-extract-plugin" );
+
+const JS_DIR = path.resolve(__dirname, '/src/js' );
+const BUILD_DIR = path.resolve(__dirname, 'build');
+
 const entry = {
-    main : require('path').resolve(__dirname, '/src/js' ) + '/index.js'
+    main : JS_DIR + '/index.js'
 };
+
 const output = {
-    path: require('path').resolve(__dirname, 'build'),
+    path: BUILD_DIR,
     filename: 'js/[name].js'
 };
+
+const rules = [
+    {
+        test: /\.js$/,
+		include: [JS_DIR],
+		exclude: /node_modules/,
+		use: "babel-loader",
+    },
+    {
+		test: /\.scss$/,
+		exclude: /node_modules/,
+		use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+	},
+];
 
 module.exports = (env, argv) => ({
     entry: entry,
     output: output,
+    devtool: 'source-map',
+
+    module: {
+        rules: rules,
+    }
 
 });
