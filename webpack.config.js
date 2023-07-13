b03ef519e15e8077a580b746be93fd0d64a01027
sarahcssiqueira <sarahcso@hotmail.com>
2023-07-13 12:39:11 -0300

    feat: basic webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,17 @@
 /**
  * Webpack configuration.
  */
+
+const entry = {
+    main : require('path').resolve(__dirname, '/src/js' ) + '/index.js'
+};
+const output = {
+    path: require('path').resolve(__dirname, 'build'),
+    filename: 'js/[name].js'
+};
+
+module.exports = (env, argv) => ({
+    entry: entry,
+    output: output,
+
+});
