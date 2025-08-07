4ae99d8cd7f5dc13d85ebb1d3e11ecaaaa5c7f11
Daniel <107655438+danielfilemon@users.noreply.github.com>
2025-08-07 17:39:05 -0300

    update

--- functions.php
+++ functions.php
@@ -1,19 +1,18 @@
 <?php
 /**
- *
  * Theme Functions
  *
- * @category Functions
- * @package  BaseWPTheme
- * @author   sarahcssiqueira
- * @license  https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html GPL
- *
+ * @package BaseWPTheme
+ * @author Sarah
+ * @license GPL-2.0-or-later
  * @link https://developer.wordpress.org/themes/basics/theme-functions/
  */
 
-/*
- * Define path constants
- */
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; 
+}
+
+
 if ( ! defined( 'BASEWPTHEME_DIR_PATH' ) ) {
 	define( 'BASEWPTHEME_DIR_PATH', untrailingslashit( get_template_directory() ) );
 }
@@ -22,9 +21,11 @@ if ( ! defined( 'BASEWPTHEME_DIR_URI' ) ) {
 	define( 'BASEWPTHEME_DIR_URI', untrailingslashit( get_template_directory_uri() ) );
 }
 
-/*
-* Enqueue Styles and Scripts
-*/
-require_once BASEWPTHEME_DIR_PATH . '/inc/classes/class-assets.php';
 
-new BaseWpTheme\Inc\Classes\Assets();
+require_once BASEWPTHEME_DIR_PATH . '/inc/classes/class-assets.php'; 
+
+
+
+if ( class_exists( 'BaseWpTheme\Inc\Classes\Assets' ) ) {
+	new BaseWpTheme\Inc\Classes\Assets();
+}
